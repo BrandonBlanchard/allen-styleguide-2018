f3f306841fba136a6719538e914b8dcd045e07a7
Brandon <brandonb@alleninstitute.org>
2018-06-27 11:52:45 -0700

    Added a flex container component. Reformatted colors page to be responsive. Added a new responsive container.

--- styleguide/models.py
+++ styleguide/models.py
@@ -8,3 +8,13 @@ class ColorSwatch(CMSPlugin):
     color = models.CharField(max_length=6, default='343434')
     name = models.CharField(max_length=20, default='new color')
     sass_name = models.CharField(max_length=20, default='$newColor')
+
+class FlexContainer(CMSPlugin):
+    CONTAINER_SPACING_TYPES = (
+        ('flex-start','flex-start'),
+        ('flex-end','flex-end'),
+        ('center','center'),
+        ('space-between','space-between'),
+        ('space-around','space-around'))
+    
+    spacing = models.CharField(choices = CONTAINER_SPACING_TYPES, default = 'flex-start', max_length = 13)
