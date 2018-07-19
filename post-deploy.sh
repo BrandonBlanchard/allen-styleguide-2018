b44d69a8dded96e370b58d61252294475fc05f6c
Brandon <brandonb@alleninstitute.org>
2018-07-19 15:15:31 -0700

    virtual env addedddddy

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,4 +1,5 @@
-python -m pip uninstall -y -r requirements.txt
+python -m pip install virtualenv
+virtualenv env
 python -m pip install -r requirements.txt
 python manage.py migrate 
 python manage.py runserver
