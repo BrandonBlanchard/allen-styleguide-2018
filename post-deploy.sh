d8ba0d91960a76d6b36ca5bc00d7e09d432d245a
Brandon <brandonb@alleninstitute.org>
2018-07-19 15:04:16 -0700

    pip uninstall needs a value for -r

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,4 +1,4 @@
-python -m pip uninstall -y -r <(python -m pip freeze)
+python -m pip uninstall -y -r requirements.txt
 python -m pip install -r requirements.txt
 python manage.py migrate 
 python manage.py runserver
